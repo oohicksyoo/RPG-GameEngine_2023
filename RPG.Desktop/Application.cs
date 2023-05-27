eac4ab004dd2e3a7392c45cdd943bcc772412f3f
Alex Hicks <alexhicks@live.ca>
2023-05-27 11:15:34 -0400

    Initial commit of structure

---
+++ RPG.Desktop/Application.cs
@@ -0,0 +1,8 @@
+﻿namespace RPG.Desktop {
+	public class Application {
+		
+		static void Main(string[] args) {
+			Console.WriteLine("Hello World");
+		}
+	}
+}
---
+++ RPG.Engine/SampleEngineClass.cs
@@ -0,0 +1,5 @@
+﻿namespace RPG.Engine {
+	public class SampleEngineClass {
+		
+	}
+}
