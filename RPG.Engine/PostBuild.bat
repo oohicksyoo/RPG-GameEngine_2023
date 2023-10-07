0a8fd3c075d01f700baeac21e5b4424882a57374
Alex Hicks <oohicksyoo@gmail.com>
2023-10-06 20:40:17 -0400

    Sample game is now setup to create a symlink of the project's asset folder after a build in the editor is created. Non editor builds will be broken for now

---
+++ RPG.Engine/PostBuild.bat
@@ -0,0 +1,18 @@
+﻿@echo ON
+
+echo Running PostBuild
+
+REM Store the Project's path that contains this script and the Assets folder we want to create a symlink for
+cd %2
+set path=%cd%
+echo %path%
+
+REM Destination folder
+cd %1
+echo Current cd = %cd%
+
+REM SETUP path to the assets folder
+set assetsPath=%path%\Assets
+echo %assetsPath%
+echo mklink /d Assets %assetsPath%
+mklink /d Assets %assetsPath%
